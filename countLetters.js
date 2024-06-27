089dcaee2a3d55580dd4ceff642bfb58fa3af777
Merak Bourque <xtremeyou@hotmail.com>
2024-06-26 22:13:57 -0400

    added new program that reads a string and tells how many times a charcter has been used

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,34 @@
 const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅  Assertion Passed:  ${actual} === ${expected}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-    }
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
 };
+
+const countLetters = function (string) {
+  const newSentence = {};
+  for (const letters of string) {
+    if (letters !== " ") {
+      if (newSentence[letters]) {
+        newSentence[letters] += 1;
+      } else {
+        newSentence[letters] = 1;
+      }
+    }
+  }
+
+  return newSentence;
+};
+
+const result = countLetters("Lighthouse in the house");
+assertEqual(result["L"], 1);
+assertEqual(result["i"], 2);
+assertEqual(result["g"], 1);
+assertEqual(result["h"], 4);
+assertEqual(result["t"], 2);
+assertEqual(result["o"], 2);
+assertEqual(result["u"], 2);
+assertEqual(result["s"], 2);
+assertEqual(result["e"], 3);
+assertEqual(result["n"], 1);
