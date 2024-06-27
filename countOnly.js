0f23614995de0a3f8d6c1b585438dee0c5cce380
Merak Bourque <xtremeyou@hotmail.com>
2024-06-26 20:57:11 -0400

    fixed code to display correct outputs and assertion tests

--- countOnly.js
+++ countOnly.js
@@ -10,12 +10,6 @@ const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-        results[item] = 1;
-      } 
     if (itemsToCount[item]) {
         if (results[item]) {
           results[item] += 1;
