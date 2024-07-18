556092a9206dac8bda057faae312469acb2b187c
Merak Bourque <xtremeyou@hotmail.com>
2024-07-17 21:56:24 -0400

    refactored functions

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,15 @@
-const assertEqual = require("./assertEqual")
+const assertEqual = require("./assertEqual");
+
 const eqArrays = function (array1, array2) {
-    if (array1.length !== array2.length) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
+  }
+  return true;
+};
 
-  module.exports = eqArrays;
+module.exports = eqArrays;
