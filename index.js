1749178133cd30ef22c537961e946aa5dad9acf1
Merak Bourque <xtremeyou@hotmail.com>
2024-07-18 18:46:21 -0400

    fixed duplicates

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const letterPositions = require("./letterPositions");
 const flatten = require("./flatten");
 const findKey = require("./findKey");
 const takeUntil = require("./takeUntil");
-const without = require("./without")
-const findKeyByValue = require("./findKeyByValue")
-const countOnly = require("./countOnly")
-const countLetters = require("./countLetters") 
-const assertArraysEqual = require("./assertArraysEqual")
-const assertEqual = require("./assertEqual")
-const assertObjectsEqual = require("./assertObjectsEqual")
-const eqArrays = require("./eqArrays")
-const eqObjects = require("./eqObjects")
-
-
-
+const without = require("./without");
+const findKeyByValue = require("./findKeyByValue");
+const countOnly = require("./countOnly");
+const countLetters = require("./countLetters");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
 
 module.exports = {
-    //can also write these with just the name and no : ...
+  //can also write these with just the name and no : ...
   head: head,
   tail: tail,
   middle: middle,
@@ -36,7 +33,4 @@ module.exports = {
   assertObjectsEqual: assertObjectsEqual,
   eqArrays: eqArrays,
   eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue
-
 };
