26206b5276b176f56132a67b5195ff6eee7fd619
Merak Bourque <xtremeyou@hotmail.com>
2024-07-18 10:42:58 -0400

    refactored the file to use mocha/chai

--- tail.js
+++ tail.js
@@ -1,8 +1,9 @@
-const assertEqual = require("./assertEqual");
 
+//returns everything on array but first element
 const tail = function(array) {
     return array.slice(1);
 };
+console.log(tail([0,1,2,3,4]));
 
 
 module.exports = tail;
