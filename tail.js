5c4a19b40d70e45cddb292e43095fa67f0ca6938
Merak Bourque <xtremeyou@hotmail.com>
2024-07-18 10:44:10 -0400

    Removed extra console.log

--- tail.js
+++ tail.js
@@ -1,9 +1,6 @@
-
 //returns everything on array but first element
-const tail = function(array) {
-    return array.slice(1);
+const tail = function (array) {
+  return array.slice(1);
 };
-console.log(tail([0,1,2,3,4]));
-
 
 module.exports = tail;
