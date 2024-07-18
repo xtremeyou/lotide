19f4fb8aae3a9ef1faa2c84cf95f6194c40df470
Merak Bourque <xtremeyou@hotmail.com>
2024-07-18 18:44:25 -0400

    Sooo many changes...

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,8 @@
 //two parameters 1, array to work with
 //t2, the callback, which lodash calls predicate
 
+const { assertArraysEqual } = require(".");
+
 
 
 const takeUntil = function(array, callback) {
@@ -22,10 +24,10 @@ module.exports = takeUntil;
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-assertEqualArrays(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-assertEqualArrays(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
