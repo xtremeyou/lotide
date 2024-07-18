4412615c3027756bdb1172f3050e09a123820484
Merak Bourque <xtremeyou@hotmail.com>
2024-07-17 21:00:44 -0400

    Refactored even more

--- head.js
+++ head.js
@@ -7,8 +7,5 @@ const head = function(array) {
     return array[0];
 
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+
 module.exports = head;
