50180ee2d8b0f934dc2a46a6957d2db44db75f8f
Merak Bourque <xtremeyou@hotmail.com>
2024-07-17 20:38:00 -0400

    Refactored functions/tests

--- middle.js
+++ middle.js
@@ -37,9 +37,5 @@ const middle = function (array) {
   }
   return midArr;
 };
-assertEqualArrays(middle([1]), []); // expected output -> []
-assertEqualArrays(middle([1, 2]), []);
-assertEqualArrays(middle([1, 2, 3]), [2]);
-assertEqualArrays(middle([1, 2, 3, 4, 5]), [3]);
-assertEqualArrays(middle([1, 2, 3, 4]), [2, 3]);
-assertEqualArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+modules.exports = middle;
